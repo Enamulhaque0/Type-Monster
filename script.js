6cc384791faa56f3ef46c39586148392ccc9db58
Enamulhaque0e <enamulhaq243@gmail.com>
2022-09-14 22:39:11 +0600

    secend fixed

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ setInterval(() => {
   
 
 
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent.toFixed(0) : 0} seconds`;
   
   
 }, 1000);
